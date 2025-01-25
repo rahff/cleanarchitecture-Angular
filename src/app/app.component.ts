ba65a6c438cb3d91427af3f7328daa6e27ace342
rahff <raphaelandrey99@gmail.com>
2025-01-25 13:31:27 +0100

    fix potential leak from Subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import {Component, effect, OnInit, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {Animal, AnimalListModel} from "../core/model";
-import {Observable} from "rxjs";
-import {toSignal} from "@angular/core/rxjs-interop";
-import {animate} from "@angular/animations";
+
 
 
 @Component({
@@ -16,26 +14,26 @@ import {animate} from "@angular/animations";
 })
 export class AppComponent implements OnInit{
   public title = 'clean-architecture';
-  public animalList = signal(this.animalListModel.getState())
+  public animalList = signal(this.animalListModel.getState());
 
   constructor(private animalListModel: AnimalListModel) {}
 
   public ngOnInit() {
     this.animalListModel.displayAll().subscribe((data) => {
-      this.animalList.set(data)
-    }).unsubscribe()
+      this.animalList.set(data);
+    }).unsubscribe();
   }
 
   public filterByDog(): void {
-    this.animalList.update((data: Animal[]) => this.animalListModel.filterAnimal("Dog"))
+    this.animalList.update((data: Animal[]) => this.animalListModel.filterAnimal("Dog"));
   }
 
   public filterByCat(): void {
-    this.animalList.update((data: Animal[]) => this.animalListModel.filterAnimal("Cat"))
+    this.animalList.update((data: Animal[]) => this.animalListModel.filterAnimal("Cat"));
   }
 
   public allAnimals(): void {
-    this.animalList.update((data: Animal[]) => this.animalListModel.getState())
+    this.animalList.update((data: Animal[]) => this.animalListModel.getState());
   }
 }
 
